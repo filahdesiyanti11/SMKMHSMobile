4c0099090dc4053daed7f7a29f20ea6133f51ed7
filah <filahdesyanti@gmail.com>
2023-10-26 12:46:08 +0700

    menyelesaikan home

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,20 +1,46 @@
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import React from 'react';
 
 const Detail = () => {
   return (
-    <View
-      style={{
-        height: 50,
-        width: 50,
-        backgroundColor: 'red',
-        marginHorizontal: 16,
-        borderRadius: 99,
-        marginTop: 16,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-      }}></View>
+    <View>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginTop: 20,
+        }}>
+        <View
+          style={{
+            height: 40,
+            width: 40,
+            backgroundColor: 'red',
+            marginHorizontal: 16,
+            borderRadius: 99,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Image
+            source={require('../assets/images/Button.png')}
+            style={{width: 40, height: 30}}
+          />
+        </View>
+        <Text
+          style={{
+            fontFamily: 'DM Sans',
+            fontSize: 14,
+            color: 'black',
+            fontWeight: 'bold',
+          }}>
+          Kembali
+        </Text>
+      </View>
+      <Image
+        source={require('../assets/images/burger.png')}
+        style={{width: 200, height: 200, alignItems: 'center'}}></Image>
+    </View>
   );
 };
 
